5fdc02267f627ee0c74ab779d1a3cfca775d5e00
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-06-13 23:03:58 -0700

    Create 141. Linked List Cycle.java

---
+++ MicroSoft/141. Linked List Cycle.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return  false;
+        }
+        
+        ListNode fast = head.next;
+        ListNode slow = head;
+        
+        while (fast != slow) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+
+2 pointers O(n) O(1)
+hashmap O(n) O(n)
