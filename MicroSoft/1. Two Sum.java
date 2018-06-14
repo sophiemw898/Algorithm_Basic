d176f3cd26350454c17a6e488e18c1308740a722
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-06-14 10:45:24 -0700

    Create 1. Two Sum.java

---
+++ MicroSoft/1. Two Sum.java
@@ -0,0 +1,23 @@
+O(n)O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return null;
+        }
+        
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        
+        return res;
+    }
+}
