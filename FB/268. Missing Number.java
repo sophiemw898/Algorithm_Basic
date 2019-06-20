b1038279579de0610ca74dfcddd32ea8daa47e1c
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-20 15:04:55 -0700

    Create 268. Missing Number.java

---
+++ FB/268. Missing Number.java
@@ -0,0 +1,15 @@
+//#1 O(n) O(n)
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
