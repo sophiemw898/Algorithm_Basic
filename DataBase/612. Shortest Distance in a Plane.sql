82f59008577df9a1d5ede04b8acb492e4e5d0e4a
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-20 11:59:41 -0700

    Create 612. Shortest Distance in a Plane.sql

---
+++ DataBase/612. Shortest Distance in a Plane.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select min(round((sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2))),2)) as shortest
+from point_2d p1 join point_2d p2
+on p1.x != p2.x or p1.y != p2.y
+
+
