bcfa02cdbdf89901952af176a50fed6492f3c02a
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-19 17:44:28 -0700

    Create 607. Sales Person.sql

---
+++ DataBase/607. Sales Person.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select name 
+from salesperson
+where sales_id not in (
+    select sales_id 
+    from orders join company
+    on orders.com_id = company.com_id
+    where name = 'RED'
+)
