44e8e58832f75c205f57e10b98fed58ea663ff99
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-21 15:23:33 -0700

    Create 206. Reverse Linked List.java

---
+++ FB/206. Reverse Linked List.java
@@ -0,0 +1,47 @@
+//recursion O(n)O(N)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return dummy;
+    }
+}
+
+//Iterative O(n)O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        return prev;
+    }
+}
