1fbb461410517737a90d4853d525fe7eacf5f48b
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-28 13:13:57 -0700

    Create 252. Meeting Rooms.java

---
+++ FB/252. Meeting Rooms.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return true;
+        }
+        
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+        
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
