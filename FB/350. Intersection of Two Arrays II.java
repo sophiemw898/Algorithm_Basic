77d64179d7c1d2bd4b25638d580616f420b44127
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-28 11:51:41 -0700

    Create 350. Intersection of Two Arrays II.java

---
+++ FB/350. Intersection of Two Arrays II.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        
+        for (int num : nums1) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                res.add(num);
+                map.put(num, map.get(num) - 1);
+            } 
+        }
+        
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
