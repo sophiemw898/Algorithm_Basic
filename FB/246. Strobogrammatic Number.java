85200b493c3128358ff0f82213d65a46d19c5eb0
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-06-29 15:14:31 -0700

    Create 246. Strobogrammatic Number.java

---
+++ FB/246. Strobogrammatic Number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        if (num == null || num.length() == 0) {
+            return true;
+        } 
+        Map<Character, Character> map = new HashMap<>();
+        map.put('0','0');
+        map.put('1','1');
+        map.put('6', '9');
+        map.put('8','8');
+        map.put('9','6');
+        
+        int left = 0, right = num.length() - 1;
+        while (left < right) {
+            if (!map.containsKey(num.charAt(left)) 
+                || map.get(num.charAt(left)) != num.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        if (left == right) {
+            return num.charAt(left) == '8' || num.charAt(left) == '1' 
+                || num.charAt(left) == '0';
+        }
+        
+        return true;
+    }
+}
