7e215d92410c6632291d505c744de38f499264b5
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-02 15:37:05 -0700

    Create 986. Interval List Intersections.java

---
+++ DataBase/986. Interval List Intersections.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> res = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < A.length && j < B.length) {
+            int start = Math.max(A[i][0], B[j][0]);
+            int end = Math.min(A[i][1], B[j][1]);
+            
+            if (start <= end) {
+                res.add(new int[] {start, end});
+            }
+            // Remove the interval with the smallest endpoint
+            if (A[i][1] < B[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        int[][] result = new int[res.size()][2];
+        int index = 0;
+        for (int[] cur : res) {
+            result[index++] = cur;
+        }
+        return result;
+    }
+}
