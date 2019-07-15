d087347f9a5a9949fc7ed0fac4bbde566a818520
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-15 13:04:15 -0700

    Update 176.Second Highest Salary.sql

--- DataBase/176.Second Highest Salary.sql
+++ DataBase/176.Second Highest Salary.sql
@@ -1,4 +1,22 @@
 # Write your MySQL query statement below
+select ifnull (
+    (select distinct salary
+    from employee
+    order by salary desc
+    limit 1,1), null) as SecondHighestSalary
+    
+# select .. from .. order by .. limit a, b 
+# select .. from .. order by .. limit b offset a
+#跳过a 取b个
+
+# Write your MySQL query statement below
+select ifnull (
+    (select distinct salary
+    from employee
+    order by salary desc
+    limit 1 offset 1), null) as SecondHighestSalary
+    
+# Write your MySQL query statement below
 select max(salary) as SecondHighestSalary
 from Employee
 where salary < (select max(salary) from Employee)
