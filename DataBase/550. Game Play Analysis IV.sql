30b2b5db87548e003c38fe994b434f6d3b50044c
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-16 00:17:52 -0700

    Update 550. Game Play Analysis IV.sql

--- DataBase/550. Game Play Analysis IV.sql
+++ DataBase/550. Game Play Analysis IV.sql
@@ -20,4 +20,11 @@ select round(
     / (select count(distinct player_id) as count from Activity),2)
 as fraction
 
+# Write your MySQL query statement below
+select
+round(sum(case when a.min +1 = a2.event_date then 1 else 0 end) / count(distinct a.player_id),2) as fraction
+from
+(select player_id, min(event_date) as min
+from activity
+group by player_id) a join activity a2 on a.player_id = a2.player_id
 
