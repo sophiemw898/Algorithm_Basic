452e59317bb67a4b67fc0b06afa5b5cdf38be37d
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-18 23:18:51 -0700

    Update 268. Missing Number.java

--- FB/268. Missing Number.java
+++ FB/268. Missing Number.java
@@ -1,3 +1,27 @@
+//O(n) O(1)
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            while (nums[i] != i && nums[i] < n) {
+                int temp = nums[i];
+                nums[i] = nums[temp];
+                nums[temp] = temp;
+            }
+            i++;
+        }
+        
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j) {
+                //System.out.println(j);
+                return j;
+            }
+        }
+        
+        return n;
+    }
+}
 //#1 O(n) O(n)
 class Solution {
     public int missingNumber(int[] nums) {
