418372b9b4c2819edc709bdea26e298b7e19583f
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-19 23:21:24 -0700

    Create 91. Decode Ways.java

---
+++ FB/91. Decode Ways.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++) {
+            if (s.charAt(i - 1) == '0') {
+                if (s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2') {
+                    dp[i] = dp[i - 2];
+                } else {
+                    return 0;
+                }
+            } else {
+                int num = 10 * (s.charAt(i - 2) - '0') + (s.charAt(i - 1) - '0');
+                if (num >= 11 && num <= 26) {
+                    dp[i] = dp[i - 2] + dp[i - 1];
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
+
+//初始化 f 数组为 0
+//若字符串中 s[i] 表示的阿拉伯数字在 1~9 范围内, f[i] += f[i-1]
+//若s[i-1]和s[i]连起来表示的数字在 10~26 范围内, f[i] += f[i-2] (若i==1, 则f[i] += 1)
+public class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[] nums = new int[s.length() + 1];
+        nums[0] = 1;
+        nums[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0') {
+                nums[i] = nums[i - 1];
+            }
+            
+            int twoDigits = (s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0';
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                nums[i] += nums[i - 2];
+            }
+        }
+        return nums[s.length()];
+    }
+}
