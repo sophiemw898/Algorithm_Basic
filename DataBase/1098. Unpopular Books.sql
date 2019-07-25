496928886c68f7e722242debb44fe1cf240bf661
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-25 16:21:50 -0700

    Update 1098. Unpopular Books.sql

--- DataBase/1098. Unpopular Books.sql
+++ DataBase/1098. Unpopular Books.sql
@@ -1,7 +1,22 @@
 # Write your MySQL query statement below
-select b.book_id, name
-from Books b left join Orders o
-on b.book_id = o.book_id and dispatch_date between '2018-06-23' and '2019-6-23' 
-where available_from < '2019-5-23' 
-group by b.book_id
-having sum(ifnull(quantity,0)) < 10
+#select book_id, name
+
+select Books.book_id, name
+from Books left join Orders 
+on Books.book_id = Orders.book_id
+and dispatch_date between '2018-06-23' and '2019-06-23'
+
+where available_from < '2019-05-23'
+#and dispatch_date between '2018-06-23' and '2019-06-23'
+
+group by book_id
+having sum(quantity) < 10 or sum(quantity) is null
+
+#having之后的表示对group之后的条件 多位sum count avg等
+#group之前限制的条件在group by之前用where显示
+
+#inner join中on 与 where没区别
+#left join中 on之用来限制join右边的table 
+#where则是限制两边的，只在join之后的table中选择 
+#所以这题里面dispatch_date的限制得在on之后，不然left join特性想要保留的左边表的id就不见了
+
