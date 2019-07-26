3dd7e646d2605e4dbedc95933a564a7ffcbf2407
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-26 00:23:56 -0700

    Update 569. Median Employee Salary.sql

--- DataBase/569. Median Employee Salary.sql
+++ DataBase/569. Median Employee Salary.sql
@@ -1,4 +1,24 @@
 # Write your MySQL query statement below
+select 
+  Employee.Id, 
+  Employee.Company, 
+  Employee.Salary
+from Employee join Employee alias
+on Employee.Company = alias.Company
+group by Employee.Company , Employee.Salary
+having sum(case when Employee.Salary = alias.Salary THEN 1 else 0 end) 
+            >= abs( #SUM(SIGN(Employee.Salary - alias.Salary)))
+                (select count(*)
+                 from employee e1 
+                 where Employee.Company = e1.Company and Employee.Salary <= e1.Salary) - 
+                (select count(*) 
+                 from employee e1 
+                 where Employee.Company = e1.Company and Employee.Salary >= e1.Salary)
+                )
+order by Employee.id
+                         
+#1应为num出现的个数
+# Write your MySQL query statement below
 select * 
 from Employee e
 where 1 >= abs((select count(*) 
