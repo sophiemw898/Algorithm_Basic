6d284842b1832c9a3435fd198178fa8c60b00e3e
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-07-28 14:44:25 -0700

    Update 200. Number of Islands.java

--- FB/200. Number of Islands.java
+++ FB/200. Number of Islands.java
@@ -1,3 +1,40 @@
+//dfs
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }   
+        return count;
+    }
+    
+    private void dfs(char[][] grid, int x, int y) {
+        int m = grid.length;
+        int n = grid[0].length; 
+        
+        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] == '0') {
+            return;
+        }
+        
+        grid[x][y] = '0';
+        dfs(grid, x - 1, y);
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+    }
+}
+
 //bfs
 class Solution {
     public int numIslands(char[][] grid) {
