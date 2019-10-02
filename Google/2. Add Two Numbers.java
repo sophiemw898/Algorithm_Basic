6ef3eb6936651c00850169eee82ed9cf0a04e0aa
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-10-02 13:16:07 -0700

    Create 2. Add Two Numbers.java

---
+++ Google/2. Add Two Numbers.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        int sum = 0;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null) {
+            sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            sum %= 10;
+            head.next = new ListNode(sum);
+            head = head.next;
+        }
+        
+        if (carry > 0) {
+            head.next = new ListNode(carry);
+        }
+        
+        return dummy.next;
+    }
+}
