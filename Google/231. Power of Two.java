137ee0c1bfcae7b7e7dacdc72b1a65c4f21d8c09
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-10-04 12:35:20 -0700

    Create 231. Power of Two.java

---
+++ Google/231. Power of Two.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        
+        return n == 1;
+    }
+}
+
+class Solution {
+  public boolean isPowerOfTwo(int n) {
+    if (n == 0) return false;
+    long x = (long) n;
+    return (x & (x - 1)) == 0;
+  }
+}
