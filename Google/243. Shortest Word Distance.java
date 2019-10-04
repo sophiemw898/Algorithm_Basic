451fbeac70e8c9a7dcc1779ccb54ad8963fa8e3d
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-10-04 12:07:15 -0700

    Create 243. Shortest Word Distance.java

---
+++ Google/243. Shortest Word Distance.java
@@ -0,0 +1,23 @@
+//one pass greedy
+class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int index1 = -1;
+        int index2 = -1;
+        int res = words.length;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                index1 = i;
+            }
+            if (words[i].equals(word2)) {
+                index2 = i;
+            }
+            
+            if (index1 != -1 && index2 != -1) {
+                res = Math.min(res, Math.abs(index1 - index2));
+            }
+        }
+        
+        return res;
+    }
+}
