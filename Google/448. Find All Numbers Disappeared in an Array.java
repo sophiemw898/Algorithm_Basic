911c0e33df4fa667aa717b986423772ab05b0c31
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-10-04 13:11:10 -0700

    Update 448. Find All Numbers Disappeared in an Array.java

--- Google/448. Find All Numbers Disappeared in an Array.java
+++ Google/448. Find All Numbers Disappeared in an Array.java
@@ -4,6 +4,43 @@ class Solution {
         //Set O(N) with extra space
         
         //O(N) no extras space 
+        //The idea is simple, if nums[i] != i + 1 and nums[i] != nums[nums[i] - 1], then we swap nums[i] with nums[nums[i] - 1], for example, nums[0] = 4 and nums[3] = 7, then we swap nums[0] with nums[3]. So In the end the array will be sorted and if nums[i] != i + 1, then i + 1 is missing.
+        // The example run as follows
+
+        // [4,3,2,7,8,2,3,1]
+        // [7,3,2,4,8,2,3,1]
+        // [3,3,2,4,8,2,7,1]
+        // [2,3,3,4,8,2,7,1]
+        // [3,2,3,4,8,2,7,1]
+        // [3,2,3,4,1,2,7,8]
+        // [1,2,3,4,3,2,7,8]
+        List<Integer> res = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+                //swap
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+}
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        //sort O(NlogN)
+        //Set O(N) with extra space
+        
+        //O(N) no extras space 
         //make the index with neg
         List<Integer> res = new ArrayList<>();
         
