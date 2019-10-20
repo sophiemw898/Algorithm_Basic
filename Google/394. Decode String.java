5dea3c958f515a52f457dd08e33b8bbb391ad32f
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-10-20 11:29:12 -0700

    Update 394. Decode String.java

--- Google/394. Decode String.java
+++ Google/394. Decode String.java
@@ -38,3 +38,38 @@ class Solution {
         return sb.toString();
     }
 }
+
+//dfs
+class Solution {
+    public String decodeString(String s) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            queue.offer(c);
+        }
+        
+        return helper(queue);
+    }
+    
+    private String helper(Queue<Character> queue) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        while (!queue.isEmpty()) {
+            char c = queue.poll();
+            if (Character.isDigit(c)) {
+                num = 10 * num + c - '0';
+            } else if (c == '[') {
+                String sub = helper(queue);
+                while (num-- > 0) {
+                    sb.append(sub);
+                }
+                num = 0;
+            } else if (c == ']') {
+                break;
+            } else {
+                sb.append(c);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
