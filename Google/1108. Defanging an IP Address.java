a326d00887c60158943fe9579fbb099f8962e433
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-10-31 15:01:22 -0700

    Create 1108. Defanging an IP Address.java

---
+++ Google/1108. Defanging an IP Address.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String defangIPaddr(String address) {
+//         StringBuilder sb = new StringBuilder();
+//         for (char c : address.toCharArray()) {
+//             if (c == '.') {
+//                 sb.append("[.]");
+//             } else {
+//                 sb.append(c);
+//             }
+//         }
+        
+//         return sb.toString();
+        
+        return address.replace(".", "[.]");
+    }
+}
