52347102d155971c86b2a9c1f335fc8ec9cdb3bf
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-11-05 15:48:52 -0800

    Create 1192. Critical Connections in a Network.java

---
+++ Amazon/OA/1192. Critical Connections in a Network.java
@@ -0,0 +1,61 @@
+class Solution {
+    
+    int time = 0;
+    
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (n <= 1) {
+            return res;
+        }
+        //use disc[] to store discovery times of visited vertices
+        //if vertice i isn't visted, disc[i] will keep -1, which is the defalt value
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);
+        //use low[] to store the lowest node id reachable from this node
+        int[] low = new int[n];
+        
+        //construct the graph
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (List<Integer> connection : connections) {
+            graph[connection.get(0)].add(connection.get(1));
+            graph[connection.get(1)].add(connection.get(0));
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (dist[i] == -1) {
+                dfs(i, res, graph, dist, low, i);
+            }
+        }
+        
+        return res;
+    }
+    
+    private void dfs(int index, List<List<Integer>> res, List<Integer>[] graph, 
+                     int[] dist, int[] low, int parent) {
+        // Initialize discovery time and low value 
+        dist[index] = low[index] = ++time;
+        for (int neighbor : graph[index]) {
+            //
+            if (neighbor == parent) {
+                continue;
+            }
+            if (dist[neighbor] == -1) {
+                dfs(neighbor, res, graph, dist, low, index);
+                // Check if the subtree rooted with v has a 
+                // connection to one of the ancestors of u 
+                low[index] = Math.min(low[index], low[neighbor]);
+                
+                // u - v is critical, there is no path for v to reach back to u or previous vertices of u
+                if (low[neighbor] > dist[index]) {
+                    res.add(Arrays.asList(index, neighbor));
+                }
+                
+            } else {
+                low[index] = Math.min(low[index], dist[neighbor]);
+            }
+        }
+    }
+}
